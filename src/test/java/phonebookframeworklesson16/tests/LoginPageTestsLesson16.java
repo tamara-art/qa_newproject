cc70cf364c9d4a2b7a3ce19e96982588137784d7
Tamara Mass <106035267+tamara-art@users.noreply.github.com>
2022-11-07 22:51:30 +0100

    Add files via upload
    add two files

--- src/test/java/phonebookframeworklesson16/tests/LoginPageTestsLesson16.java
+++ src/test/java/phonebookframeworklesson16/tests/LoginPageTestsLesson16.java
@@ -14,21 +14,21 @@ public class LoginPageTestsLesson16 extends TestBaseLesson16 {
     UserLesson16 invalidEmailUser = new UserLesson16("invalid_email@gmail.com", "ZZxcv_1!");
 
 
-    @Test(dataProvider = "newUsersFromFile", dataProviderClass = DataProvidersLesson16.class)
-    public void testLoginFormWithDataProviderFromFilePositive(UserLesson16 user) throws InterruptedException {
-        app.getLoginPageHelper().fillAndSubmitLoginForm(user);
-        Assert.assertTrue(app.getMenuHelper().isUserSignedIn(), "Log Out button is present");
-        TimeUnit.SECONDS.sleep(2);
-    }
-
-    @Test(dataProvider = "newUsers", dataProviderClass = DataProvidersLesson16.class)
-    public void testLoginFormWithDataProviderPositive(UserLesson16 user) throws InterruptedException {
-        app.goToLoginPage();
-        app.waitForLoginPageToLoad();
-        app.getLoginPageHelper().fillAndSubmitLoginForm(invalidPwdUser);
-        app.getLoginPageHelper().fillAndSubmitLoginForm(user);
-        Assert.assertTrue(app.getMenuHelper().isUserSignedIn(), "Log Out button is present");
-    }
+//    @Test(dataProvider = "newUsersFromFile", dataProviderClass = DataProvidersLesson16.class)
+//    public void testLoginFormWithDataProviderFromFilePositive(UserLesson16 user) throws InterruptedException {
+//        app.getLoginPageHelper().fillAndSubmitLoginForm(user);
+//        Assert.assertTrue(app.getMenuHelper().isUserSignedIn(), "Log Out button is present");
+//        TimeUnit.SECONDS.sleep(2);
+//    }
+//
+//    @Test(dataProvider = "newUsers", dataProviderClass = DataProvidersLesson16.class)
+//    public void testLoginFormWithDataProviderPositive(UserLesson16 user) throws InterruptedException {
+//        app.goToLoginPage();
+//        app.waitForLoginPageToLoad();
+//        app.getLoginPageHelper().fillAndSubmitLoginForm(invalidPwdUser);
+//        app.getLoginPageHelper().fillAndSubmitLoginForm(user);
+//        Assert.assertTrue(app.getMenuHelper().isUserSignedIn(), "Log Out button is present");
+//    }
 
     @Test
     public void testLoginFormPositive() {
@@ -53,23 +53,24 @@ public class LoginPageTestsLesson16 extends TestBaseLesson16 {
         Assert.assertTrue(app.getLoginPageHelper().isLoginErrorMessagePresent("Login Failed with code 400"), "Login error message is present");
         Assert.assertFalse(app.getMenuHelper().isUserSignedIn(), "User is not signed in");
     }
-
-    @Test
-    public void testLoginFormWithRecording() {
-        UserLesson16 invalidPwdUser = new UserLesson16("michael@gmail.com", "invalid_pwd");
-//        app.startRecording();
-        app.deleteRecordings();
-        app.startRecordingWithoutSound();
-        app.waitForLoginPageToLoad();
-        app.getLoginPageHelper().fillAndSubmitLoginForm(invalidPwdUser);
-        app.sleep(1);
-        app.getLoginPageHelper().clickOnErrorAlert();
-        Assert.assertTrue(app.getLoginPageHelper().isLoginErrorMessagePresent("Login Failed with code 400"), "Login error message is present");
-        Assert.assertFalse(app.getMenuHelper().hasSignOutButtonPresent(), "User is not signed in");
-        app.sleep(2);
-        app.stopRecording();
-    }
 }
+
+//    @Test
+//    public void testLoginFormWithRecording() {
+//        UserLesson16 invalidPwdUser = new UserLesson16("michael@gmail.com", "invalid_pwd");
+////        app.startRecording();
+//        app.deleteRecordings();
+//        app.startRecordingWithoutSound();
+//        app.waitForLoginPageToLoad();
+//        app.getLoginPageHelper().fillAndSubmitLoginForm(invalidPwdUser);
+//        app.sleep(1);
+//        app.getLoginPageHelper().clickOnErrorAlert();
+//        Assert.assertTrue(app.getLoginPageHelper().isLoginErrorMessagePresent("Login Failed with code 400"), "Login error message is present");
+//        Assert.assertFalse(app.getMenuHelper().hasSignOutButtonPresent(), "User is not signed in");
+//        app.sleep(2);
+//        app.stopRecording();
+//    }
+//}
 
 //    @Test
 //    public void testLoginFormWithInvalidEmailNegative() {
